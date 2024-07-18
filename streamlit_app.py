964c9aebbfbd84bd7bcc41e4e54c4ec12e37d6c9
agithub-user <sharonlu314@gmail.com>
2024-07-17 22:02:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,14 @@ hide_streamlit_style = """
     """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # Set OpenAI API key
 openai.api_key = st.secrets.openai_key
 
