dca6d294beafa7e95d42da515388a646ff30d4be
agithub-user <sharonlu314@gmail.com>
2024-07-17 17:31:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 # Set Streamlit page config
 st.set_page_config(page_title="Share-On chatbot.", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# Custom CSS to change the background color to light blue
+# Custom CSS to change the background color to aqua blue
 custom_css = """
     <style>
     html, body, [data-testid="stAppViewContainer"], [data-testid="stHeader"], [data-testid="stSidebar"] {
